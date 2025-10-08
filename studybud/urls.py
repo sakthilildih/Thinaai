ce94bee7cf77b91b93fc0d1e0f21cbfbfb4ba79d
sakthilildih <sakthilildih@gmail.com>
2025-10-08 18:10:03 +0530

    homepage updtae

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Landing page at root
+    
+    # App routes
     path('', include('base.urls')),
     path('api/', include('base.api.urls'))
 ]
