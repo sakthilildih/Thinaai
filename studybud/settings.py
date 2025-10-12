5f8adbca59d2e8a67ea8782a51341c801a9b1edc
sakthilildih <sakthilildih@gmail.com>
2025-10-12 17:09:34 +0530

    Update settings.py for DigitalOcean deployment

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-localhost-development-key-change-in-production')
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = False
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,0.0.0.0').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 
